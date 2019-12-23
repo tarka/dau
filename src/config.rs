cb42e04509c6286d04971964c3ccfc05c72f92e3
Steve Smith <tarkasteve@gmail.com>
2019-12-23 14:08:32 +1100

    Add start of config file format and parser.

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,8 @@
 
+use std::collections::HashMap;
+use serde::{self, Deserialize};
+use toml;
+
 use crate::errors::Result;
 
 pub const CONFFILE: &'static str = "/etc/dau.toml";
@@ -11,4 +15,84 @@ pub fn default_priv_groups() -> Result<Vec<String>> {
        .split(":")
        .map(String::from)
        .collect())
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
+#[serde(rename_all = "lowercase")]
+enum Type  {
+    User,
+    Group
+}
+impl Default for Type {
+    fn default() -> Self { Type::User }
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(default)]
+struct Config {
+    timeout: String,
+    #[serde(flatten)]
+    perms: HashMap<String, Perm>,
+}
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            timeout: "5m".to_string(),
+            perms: HashMap::new(),
+        }
+    }
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(default)]
+struct Perm {
+    all: bool,
+    #[serde(alias = "type")]
+    ptype: Type,
+    commands: Vec<String>,
+}
+impl Default for Perm {
+    fn default() -> Self {
+        Self {
+            all: false,
+            ptype: Type::User,
+            commands: vec![],
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn toml_default() {
+        let config: Config = toml::from_str("").unwrap();
+        assert_eq!("5m", config.timeout);
+        assert_eq!(0, config.perms.len());
+    }
+
+    #[test]
+    fn toml_test() {
+        let s = "timeout = '30s'
+
+                 [testuser]
+                 all = true
+
+                 [testgroup]
+                 type = 'group'
+                 all = true
+
+                 [limiteduser]
+                 commands = ['/bin/ls']
+                 ";
+        let config: Config = toml::from_str(s).unwrap();
+        println!("CONF: {:?}", config);
+        assert_eq!("30s", config.timeout);
+        assert!(config.perms["testuser"].all);
+        assert_eq!(Type::Group, config.perms["testgroup"].ptype);
+        assert!(!config.perms["limiteduser"].all);
+        assert_eq!(vec!("/bin/ls"), config.perms["limiteduser"].commands);
+    }
 }
